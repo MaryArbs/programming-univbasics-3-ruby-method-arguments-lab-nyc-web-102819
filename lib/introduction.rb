61d280a1c606cf09bbe36d745bc0255aa0df0c30
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-10-04 00:46:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,7 @@ introduction ("Josh")
 def introduction_with_language (name, language= 'Ruby')
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language ('Dan', 'Ember.js')
+introduction_with_language ("Dan", "Ember.js")
 
 
 
